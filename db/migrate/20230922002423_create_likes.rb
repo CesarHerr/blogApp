5b227a5cfdf9131a16bcba3088afcee7a0c255bf
Cesar Herrera <cesarherr321@gmail.com>
2023-09-21 22:27:26 -0300

    Create schema, add migrations

--- db/migrate/20230922002423_create_likes.rb
+++ db/migrate/20230922002423_create_likes.rb
@@ -5,7 +5,7 @@ class CreateLikes < ActiveRecord::Migration[7.0]
       t.integer :post_id
       t.timestamps
     end
-    foreign_key :likes, :users, column: :user_id
-    foreign_key :likes, :posts, column: :post_id
+    add_foreign_key :likes, :users, column: :user_id
+    add_foreign_key :likes, :posts, column: :post_id
   end
 end
