5b997da3658592802426cc796c081253c80b4c5e
Cesar Herrera <cesarherr321@gmail.com>
2023-09-23 01:59:40 -0300

    Add callbacks for comments_counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :user_id
   belongs_to :post
 
+  after_create :comments_counter
+  after_destroy :comments_counter
+
   def comments_counter
     post.update_column(:comments_counter, post.comments.count)
   end
