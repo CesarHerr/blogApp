6585e41356014858af0a619833f65e262b9fbf86
Cesar Herrera <cesarherr321@gmail.com>
2023-09-23 01:59:11 -0300

    Add callbacks for  user_posts_count

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,9 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: :post_id
   has_many :likes, foreign_key: :post_id
 
+  after_create :user_posts_count
+  after_destroy :user_posts_count
+
   def user_posts_count
     author.update_column(:posts_counter, author.posts.count)
   end
