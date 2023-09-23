f09f869776c2cd49ae205c46ac976f2b3c475ef6
Cesar Herrera <cesarherr321@gmail.com>
2023-09-23 02:00:08 -0300

    Add callbacks for likes_counter

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,9 @@ class Like < ApplicationRecord
   belongs_to :author
   belongs_to :posts
 
+  after_create :likes_counter
+  after_destroy :likes_counter
+
   def likes_counter
     post.update_column(:likes_counter, post.likes.count)
   end
