76ab6da9a4cd58699ae871b7a04c263c5d3d7be2
Cesar Herrera <cesarherr321@gmail.com>
2023-09-23 19:05:46 -0300

    Install and configure RSpec

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_22_002423) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_23_180954) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,8 +33,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_22_002423) do
     t.integer "author_id"
     t.string "title", limit: 100
     t.string "text", limit: 1000
-    t.integer "comments_counter"
-    t.integer "likes_counter"
+    t.integer "comments_counter", default: 0
+    t.integer "likes_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -43,14 +43,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_22_002423) do
     t.string "name", limit: 50
     t.binary "photo"
     t.text "bio"
-    t.integer "posts_counter"
+    t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
-  add_foreign_key "likes", "posts"
-  add_foreign_key "likes", "users"
-  add_foreign_key "posts", "users", column: "author_id"
 end
