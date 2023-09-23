a0962dcba6f9213226a0bbe4f2285613ab1dad8f
Cesar Herrera <cesarherr321@gmail.com>
2023-09-22 21:16:37 -0300

    Add fix associations and foreign_keys

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-  belongs_to :users
-  belongs_to :posts
+  belongs_to :author, class_name: 'User', foreign_key: :user_id
+  belongs_to :post
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ApplicationRecord
-  belongs_to :users
+  belongs_to :author
   belongs_to :posts
 end
