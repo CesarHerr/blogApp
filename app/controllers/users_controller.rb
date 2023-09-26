f18be0d6dfed625885386c7de6e8a1c1583ae1e1
Cesar Herrera <cesarherr321@gmail.com>
2023-09-26 00:03:41 -0300

    Fix linters errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,9 @@
 class UsersController < ApplicationController
-  def index
-  end
+  def index; end
 
-  def show
-  end
+  def show; end
 
-  def user_post
-  end
+  def user_post; end
 
-  def user_post_show
-  end
+  def user_post_show; end
 end
