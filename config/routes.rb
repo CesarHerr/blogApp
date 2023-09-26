c5a37cdc5101a78756c973f56e7fd5c2b657d2e1
Cesar Herrera <cesarherr321@gmail.com>
2023-09-26 01:11:57 -0300

    Update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  root 'users#index'
-  get 'users/:id', to: 'users#show'
-
-  get 'users/:id/posts', to: 'posts#index'
-  get 'users/:id/posts/:post_id', to: 'posts#show'
+    resources :users, only: [:index, :show] do
+      resources :posts, only: [:index, :show]
+  end
 end
