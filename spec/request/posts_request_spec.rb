996978172400aa29c0b31198b5e08e174983ca41
Cesar Herrera <cesarherr321@gmail.com>
2023-09-27 20:10:40 -0300

    Add test for posts controller

---
+++ spec/request/posts_request_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper.rb'
+
+RSpec.describe 'Posts', type: :request do
+  describe 'Get /index' do
+    it 'returns http success' do
+      get '/users/:user_id/posts'
+      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(200)
+    end
+
+    it 'returns the expected content' do
+      get '/users/:user_id/posts'
+      expect(response.body).to include('This is Post index')
+    end
+  end
+
+  describe 'Get /show' do
+    it 'returns http success' do
+      get '/users/:user_id/posts/:post_id'
+      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(200)
+    end
+
+    it 'returns the expected content' do
+      get '/users/:user_id/posts/:post_id'
+      expect(response.body).to include('This is Post show')
+    end
+  end
+end
