f0397c42cf68e62b3142263bb743047ad106e897
Cesar Herrera <cesarherr321@gmail.com>
2023-09-27 21:56:58 -0300

    Add render test for post controller

--- spec/request/posts_request_spec.rb
+++ spec/request/posts_request_spec.rb
@@ -8,6 +8,11 @@ RSpec.describe 'Posts', type: :request do
       expect(response).to have_http_status(200)
     end
 
+    it 'renders the index template' do
+      get "/users/:user_id/posts"
+      expect(response).to render_template('index')
+    end
+
     it 'returns the expected content' do
       get '/users/:user_id/posts'
       expect(response.body).to include('This is Post index')
@@ -19,6 +24,11 @@ RSpec.describe 'Posts', type: :request do
       get '/users/:user_id/posts/:post_id'
       expect(response).to have_http_status(:success)
       expect(response).to have_http_status(200)
+    end
+
+    it 'renders the show template' do
+      get "/users/:user_id/posts/:post_id"
+      expect(response).to render_template('show')
     end
 
     it 'returns the expected content' do
