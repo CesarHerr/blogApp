dc1c9c8fbb1546879cadd4e5be195f066bbe1a9e
Cesar Herrera <cesarherr321@gmail.com>
2023-09-27 21:54:09 -0300

    Add render test for user controller

--- spec/request/users_request_spec.rb
+++ spec/request/users_request_spec.rb
@@ -8,6 +8,11 @@ RSpec.describe 'Users', type: :request do
       expect(response).to have_http_status(200)
     end
 
+    it 'renders the index template' do
+      get "/users"
+      expect(response).to render_template('index')
+    end
+
     it 'returns the expected content' do
       get '/users'
       expect(response.body).to include('This is User index')
@@ -19,6 +24,11 @@ RSpec.describe 'Users', type: :request do
       get '/users/:user_id'
       expect(response).to have_http_status(:success)
       expect(response).to have_http_status(200)
+    end
+
+    it 'renders the show template' do
+      get '/users/:user.id'
+      expect(response).to render_template('show')
     end
 
     it 'returns the expected content' do
