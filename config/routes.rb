944a9d7dbab50dc964984bc8d3b7455d6ae41a94
Cesar Herrera <cesarherr321@gmail.com>
2023-09-30 17:41:11 -0300

    Change likes route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
-      post 'like', to: 'posts#like'
+      resources :likes, only: [:create]
     end
   end
 end
