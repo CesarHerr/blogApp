a761ffb736883be5b7846628afed0e02e8354f65
Cesar Herrera <cesarherr321@gmail.com>
2023-09-30 17:40:37 -0300

    Refactor Likes in a new Controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,19 +24,6 @@ class PostsController < ApplicationController
     end
   end
 
-  def like
-    @post = Post.find(params[:id])
-    @like = Like.new(post: @post, author: current_user)
-
-    if @like.save
-      flash[:notice] = 'You have liked the post!.'
-    else
-      flash[:alert] = "Something is wrong!, you don't liked the post!."
-    end
-
-    redirect_to user_post_path(current_user.id, @post.id)
-  end
-
   def post_params
     params.require(:post).permit(:title, :text)
   end
