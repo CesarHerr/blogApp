f33a5310faf4dbd93d33db052177d6835557ebc6
Cesar Herrera <cesarherr321@gmail.com>
2023-09-29 23:32:30 -0300

    Add new, create and like methods to post contr.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,43 @@
 class PostsController < ApplicationController
   def index
     @users = User.all
-    @posts = Post.all
   end
 
   def show
     @post = Post.find(params[:id])
     @comments = Comment.all
+  end
+
+  def new
+    @user = current_user
+    @new_post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      flash[:notice] = 'Post successfully added!'
+      redirect_to user_path(current_user)
+    else
+      flash[:alert] = 'Post could not be added.'
+      redirect_to new_user_post_path(current_user)
+    end
+  end
+
+  def like
+    @post = Post.find(params[:id])
+    @like = Like.new(post: @post, author: current_user)
+
+    if @like.save
+      flash[:notice] = 'You have liked the post!.'
+    else
+      flash[:alert] = "Something is wrong!, you don't liked the post!."
+    end
+
+    redirect_to user_post_path(current_user.id, @post.id)
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
