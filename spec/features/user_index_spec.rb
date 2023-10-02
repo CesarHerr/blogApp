38d84a30b900f70e5364d90c96bd81e0f3922999
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-02 17:23:47 -0400

    Create test to check the username of all users in the users' index page

---
+++ spec/features/user_index_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "Visit the users' index page", type: :feature do
+  before :each do
+    @user1 = User.create(name: 'Nick Jhons', photo: 'https://randomuser.me/api/portraits/men/51.jpg', bio: 'I am a software engineer with more than 3 years of experience on web development.')
+    @user2 = User.create(name: 'Jenna Lenz', photo: 'https://randomuser.me/api/portraits/women/54.jpg', bio: 'My name is Jenna Lenz, I am a Frontend Developer with a vast experience on React and Angular frameworks.')
+  end
+
+  it "should show the username of all the users listed on the page" do
+    visit users_path
+    expect(page).to have_content 'Nick Jhons'
+    expect(page).to have_content 'Jenna Lenz'
+  end
+end
