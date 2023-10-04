055249a83d73fbbe844a9ea4a27d2eec9cd3e87e
Cesar Herrera <cesarherr321@gmail.com>
2023-10-03 21:55:15 -0300

    Refactor and fix migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_23_180954) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_04_001514) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,4 +48,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_23_180954) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users", column: "author_id"
 end
