8470169d16efcaeb1958ade8c9a564223883d782
Cesar Herrera <cesarherr321@gmail.com>
2023-10-06 16:29:47 -0300

    Add destroy action  on comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
-  before_action :find_post_by_id, only: %i[new create]
+  before_action :authenticate_user!, only: %i[new create]
+  before_action :find_post_by_id, only: %i[new create destroy]
 
   def new
     @user = current_user
@@ -18,6 +19,17 @@ class CommentsController < ApplicationController
       flash[:alert] = 'Comment could not be added.'
       redirect_to new_user_post_comment_path(@user, @post)
     end
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    authorize! :destroy, comment
+    if comment.destroy
+      flash[:notice] = 'The Post was deleted successfully!'
+    else
+      flash[:error] = 'Error! The Post was not deleted'
+    end
+    redirect_to user_post_path(@post.author, @post)
   end
 
   private
